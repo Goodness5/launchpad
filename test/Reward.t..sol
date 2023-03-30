c20b04383bbea40aeec8327ed4c2e66c8441c399
goodness5 <goodnesskolapo@gmail.com>
2023-03-30 19:29:51 +0100

    hmmm

--- test/Reward.t..sol
+++ test/Reward.t..sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../lib/forge-std/src/Test.sol";
+import "../src/reward.sol";
+import "../src/token.sol";
+
 
 contract CounterTest is Test {
     Counter public counter;
